ac2bca69ca67232382ee1420f98a786a9d617d30
Xencute <153698086+Xencute@users.noreply.github.com>
2023-12-28 20:26:09 +0700

    Update Buoi5_practice2.sql

--- Buoi5_practice2.sql
+++ Buoi5_practice2.sql
@@ -19,3 +19,37 @@ HAVING count(skill) >=3
 ORDER BY candidate_id
 ---Bai tap 6 --- skip buoi sau
 ---Bai tap 7---
+SELECT card_name,
+max(issued_amount)-min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
+order by max(issued_amount)-min(issued_amount) DESC
+---Bai tap 8---
+SELECT manufacturer,
+COUNT(drug) as failed_product,
+abs(SUM(total_sales-cogs)) as total_loss
+FROM pharmacy_sales
+WHERE total_sales- cogs <0
+GROUP BY manufacturer
+ORDER BY total_loss desc
+---Bai tap 9---
+select id, movie, description, rating
+from cinema
+where description !='boring' and id%2!=0
+order by rating desc
+---Bai tap 10---
+Select teacher_id,
+count (distinct subject_id)
+from teacher
+group by teacher_id
+---Bai tap 11---
+select user_id,
+count(follower_id) as tol_follower
+from followers
+group by user_id
+order by user_id
+---Bai tap 12---
+select class
+ from courses
+ group by class
+having count(student) >=5
