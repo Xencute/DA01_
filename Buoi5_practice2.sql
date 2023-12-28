f6b9bfffa8aa7d21aacd7a3017566d35b0996dd1
Xencute <153698086+Xencute@users.noreply.github.com>
2023-12-28 19:07:01 +0700

    Update Buoi5_practice2.sql

--- Buoi5_practice2.sql
+++ Buoi5_practice2.sql
@@ -1,1 +1,21 @@
-
+---Bai tap 1----
+select distinct city from station
+where id%2=0
+---Bai tap 2--- 
+select
+count(city)- count(distinct city)
+from station
+---Bai tap 3--- skip buoi sau
+---Bai tap 4---
+SELECT 
+ROUND(SUM(ITEM_COUNT::DECIMAL * order_occurrences)/SUM(order_occurrences),1) as mean
+FROM items_per_order;
+--Bai tap 5---
+SELECT candidate_id
+FROM candidates
+where skill in ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING count(skill) >=3
+ORDER BY candidate_id
+---Bai tap 6 --- skip buoi sau
+---Bai tap 7---
