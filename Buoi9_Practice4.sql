891474449d5067273417bc5441a14043ab30b2ec
Xencute <153698086+Xencute@users.noreply.github.com>
2023-12-30 17:24:42 +0700

    Update Buoi9_Practice4.sql

--- Buoi9_Practice4.sql
+++ Buoi9_Practice4.sql
@@ -1,1 +1,17 @@
-
+---Baitap1---
+select 
+sum(case when device_type ='laptop'then 1
+else 0 
+end) as laptop_reviews,
+sum(case when device_type in ('tablet','phone') then 1
+else 0 
+end)as mobile_views
+from viewership
+----Baitap2----
+select x,y,z,
+case 
+when abs(x-y)<z and z<x+y then 'Yes'
+else 'No'
+end as triangle
+ from triangle
+---Baitap3---
