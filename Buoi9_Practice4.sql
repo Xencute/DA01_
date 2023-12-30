b6959f40fd143d7ec3158ed5b55a6f61bdf57c7b
Xencute <153698086+Xencute@users.noreply.github.com>
2023-12-30 18:04:24 +0700

    Update Buoi9_Practice4.sql

--- Buoi9_Practice4.sql
+++ Buoi9_Practice4.sql
@@ -15,3 +15,26 @@ else 'No'
 end as triangle
  from triangle
 ---Baitap3---
+select 
+round(
+CAST(COUNT(call_id) as decimal)*100/COUNT(*),1)
+from callers
+WHERE call_category is NULL or call_category='n/a'
+---Baitap4---
+select name
+from customer
+where 
+coalesce(referee_id,0)!=2
+---Baitap5---
+select survived,
+sum(case when pclass=1 then 1
+else 0
+end) as first_class,
+sum(case when pclass=2 then 1
+else 0
+end) as second_class,
+sum(case when pclass=3 then 1
+else 0
+end) as third_class
+from titanic
+group by survived
