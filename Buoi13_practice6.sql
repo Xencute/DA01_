d4f3b95387357dcfb4361f96dc7575925e70fc5e
Xencute <153698086+Xencute@users.noreply.github.com>
2024-01-07 21:35:10 +0700

    Update Buoi13_practice6.sql

--- Buoi13_practice6.sql
+++ Buoi13_practice6.sql
@@ -1,15 +1,11 @@
 ---Baitap1---
-with trung_job as
-(select company_id, title
-from job_listings
-GROUP BY title, company_id
-having COUNT(*)>1),
+with duplicate_listing as
 
-trung_jobdescription as
-(select company_id, description
-from job_listings
-GROUP BY description, company_id
-having COUNT(*)>1)
+(select company_id from job_listings
+group by company_id, title, description
+having count(*)>1)
 
-select COUNT(DISTINCT a.company_id) from job_listings as a inner join trung_job b on a.company_id=b.company_id
-inner JOIN trung_jobdescription as c on a.company_id=c.company_id
+select COUNT(*) from duplicate_listing
+
+
+---Baitap2---
